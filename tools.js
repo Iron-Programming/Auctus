343887d1fc02b73b12d5631c27cc8d09b53ba3b5
Nathan Cochran <nacochranpb@gmail.com>
2022-09-11 21:01:04 -0500

    Skeleton V.1
    Working on the outline of the Website

---
+++ tools.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*
+    File: tools.js
+    Description: Provides general JS tools for all the pages, 
+                 such as for scene management purposes
+    Autors: Nathan Cochran & Oren Kirchoff
+    Date: 2022-09-11
+*/
+
