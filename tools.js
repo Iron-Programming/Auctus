ebc5a503f501fc8cee7909950392be16f51f4f19
Nathan Cochran <58408117+Iron-Programming@users.noreply.github.com>
2022-10-06 11:47:37 -0500

    Got Basic Login Functionality

--- tools.js
+++ tools.js
@@ -16,24 +16,50 @@ var users = {
     }
 };
 
+// @function resolveSignin()
+// Credentials are either successful (send user to their profile), or incorrent (give error message)
+// username is email in this case
 function resolveSignin() {
     var username = document.getElementById("inputEmail").value;
     var password = document.getElementById("inputPassword").value;
 
-    var found = false;
+    var found = false, match = false;
 
     // 1. Search User Database
     Object.keys(users).forEach(function(key) {
-        if (username === key && password == users[key].password) {
+        if (username === key) {
             found = true;
+            match = (password == users[key].password) ? true : false;
             return;
         }
     });
 
     // 2. Handle login
-    if (found) {
-        
-    } else {
+    // #1 --> username 
+    var messages = document.getElementsByClassName('no-match');
+    messages[0].style.display = "none";
+    messages[1].style.display = "none";
+    var messages = document.getElementsByClassName('no-account');
+    messages[0].style.display = "none";
+    if (found && match) {
+        // update login status
+        localStorage.loggedin = true;
 
+        // sign in user
+        if (localStorage.user) {
+           localStorage.removeItem("user");
+        }
+        localStorage.setItem("user", JSON.stringify(users[username]));
+        window.location.href = "yourprofile.html";
+    } else if (found) {
+        var messages = document.getElementsByClassName('no-match');
+        messages[0].style.display = "block";
+        messages[1].style.display = "block";
+        messages[0].style.animation = "shake 0.5s";
+        messages[1].style.animation = "shake 0.5s";
+    } else {
+        var messages = document.getElementsByClassName('no-account');
+        messages[0].style.display = "block";
+        messages[0].style.animation = "shake 0.5s";
     }
 }
