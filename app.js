5ab33b76382b530b15fa17dedf63420aed5bb635
driftwood8891 <95161085+driftwood8891@users.noreply.github.com>
2022-10-15 11:51:48 -0500

    Building login system(backend)
    adding action and method options in our input tags.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");         // Required to run express
-const app = express();                      // Creating express object
+const express = require("express"); // Required to run express
+const app = express(); // Creating express object
 const request = require("request");
-const bodyParser = require("body-parser");  // For parsing JSON data
-const https = require("https");             // For making https requests
-const mongoose = require("mongoose");       // Setting up Mongoose DB
+const bodyParser = require("body-parser"); // For parsing JSON data
+const https = require("https"); // For making https requests
+const mongoose = require("mongoose"); // Setting up Mongoose DB
 
-app.use(express.static("public"));          // Public folder that hold all of our static resources.  The server pulls from this.
+app.use(express.static("public")); // Public folder that hold all of our static resources.  The server pulls from this.
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
@@ -17,49 +17,40 @@ const router = express.Router();
 //   res.send("Terms of use page");
 // });
 /////////////////////////////////////////////////////////////////////////////////////////
-                    ////// Database Setup(Mongoose) \\\\\\
+////// Database Setup(Mongoose) \\\\\\
 /////////////////////////////////////////////////////////////////////////////////////////
 
- // mongoose.connect("mongodb+srv://admin-oren:Hoolibah88@cluster0.9ryp70x.mongodb.net/?retryWrites=true&w=majority", {      // Connecting the database mongoose
- //   useNewUrlParser: true
- // });
+// mongoose.connect("mongodb+srv://admin-oren:Hoolibah88@cluster0.9ryp70x.mongodb.net/?retryWrites=true&w=majority", {      // Connecting the database mongoose
+//   useNewUrlParser: true
+// });
 
 mongoose.connect("mongodb://localhost:27017/Logins", {
   useNewUrlParser: true
 });
 
-// const TestSchema = {               // Creating the Schema
-//   name: String
-// };
+const namesSchema = { // Creating the Schema
+  name: String
+};
 
-// const Item = mongoose.model("Item", TestSchema);     // Creating the model
+const Item = mongoose.model("Item", namesSchema); // Creating the model
 
-const Schema = mongoose.Schema;
-
-const SomeModelSchema = new Schema({
-  a_string: String
+const item1 = new Item({ // Creating documents
+  name: "Billy"
 });
 
-// Compile model from schema
-const SomeModel = mongoose.model("SomeModel", SomeModelSchema);
-
-const item1 = new SomeModel ({                             // Creating documents
-  name: "This is a string!"
+const item2 = new Item({
+  name: "Bobby"
 });
 
-const item2 = new SomeModel ({
-  name: "Another string"
-});
-
-const item3 = new SomeModel ({
-  name: "And another"
+const item3 = new Item({
+  name: "Jimmy"
 });
 
 
 const defaultItems = [item1, item2, item3];
 
-SomeModel.insertMany(defaultItems, function(err){
-  if (err){
+Item.insertMany(defaultItems, function(err) {
+  if (err) {
     console.log(err);
   } else {
     console.log("Successfully added documents to database!");
@@ -69,44 +60,44 @@ SomeModel.insertMany(defaultItems, function(err){
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-app.get("/", function(req, res){            // get function that takes you to the home page
+app.get("/", function(req, res) { // get function that takes you to the home page
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/signin", function(req, res){
+app.get("/signin", function(req, res) {
   res.sendFile(__dirname + "/signin.html");
   // res.send("signin");
 });
 
-app.get("/signup", function(req, res){
+app.get("/signup", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.get("/termsofuse", function(req, res){
+app.get("/termsofuse", function(req, res) {
   res.sendFile(__dirname + "/termsofuse.html");
 });
 
-app.get("/privacypolicy", function(req, res){
+app.get("/privacypolicy", function(req, res) {
   res.sendFile(__dirname + "/privacypolicy.html");
 });
 
-app.get("/documentation", function(req, res){                 // CSS not loading
+app.get("/documentation", function(req, res) { // CSS not loading
   res.sendFile(__dirname + "/documentation.html");
 });
 
-app.get("/account", function(req, res){
+app.get("/account", function(req, res) {
   res.sendFile(__dirname + "/account.html");
 });
 
-app.get("/marketplace", function(req, res){           // CSS not loading
+app.get("/marketplace", function(req, res) { // CSS not loading
   res.sendFile(__dirname + "/marketplace.html");
 });
 
-app.get("/yourprofile", function(req, res){
+app.get("/yourprofile", function(req, res) {
   res.sendFile(__dirname + "/yourprofile.html");
 });
 
-app.get("/dashboard", function(req, res){
+app.get("/dashboard", function(req, res) {
   res.sendFile(__dirname + "/dashboard.html");
 });
 
@@ -118,6 +109,6 @@ app.get("/dashboard", function(req, res){
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("The server is up and running on localhost:3000 and is also set up for heroku");      // Server function
+app.listen(process.env.PORT || 3000, function() {
+  console.log("The server is up and running on localhost:3000 and is also set up for heroku"); // Server function
 });
