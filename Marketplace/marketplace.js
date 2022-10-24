db0642c651646bfb7b8ed63bdba6f69c4c8f42b1
Nathan Cochran <nacochranpb@gmail.com>
2022-10-23 22:55:28 -0500

    Worked on Marketplace.html

--- Marketplace/marketplace.js
+++ Marketplace/marketplace.js
@@ -6,9 +6,9 @@ function MarketItem(config) {
     this.location = config.author || {};
 }
 
-MarketItem.prototype.addToMarket = function() {
+MarketItem.prototype.addToMarket = function(marketID) {
     // reference parent container
-    var marketContainer = document.getElementById("market-container");
+    var marketContainer = document.getElementById(marketID);
 
     // create item div
     var itemDiv = document.createElement("div");
@@ -47,15 +47,46 @@ MarketItem.prototype.addToMarket = function() {
     marketContainer.appendChild(itemDiv);
 };
 
-var marketItems = [ ];
 
-marketItems.push(new MarketItem({
+/**********************************
+ * Suggested Market Items
+**********************************/
+var marketItems1 = [ ];
+for (var i = 0; i < 10; i++) {
+    marketItems1.push(new MarketItem({
+        name: "Item Name",
+        description: "This is a cool item.",
+        author: {},
+        location: {}
+    }));
+}
+for (var i = 0; i < marketItems1.length; i++) {
+    marketItems1[i].addToMarket("market-container-suggested");
+}
+
+
+/**********************************
+ * Trending Market Items
+**********************************/
+var marketItems2 = [ ];
+marketItems2.push(new MarketItem({
     name: "Item Name",
     description: "This is a cool item.",
     author: {},
     location: {}
 }));
-
-for (var i = 0; i < marketItems.length; i++) {
-    marketItems[i].addToMarket();
+marketItems2.push(new MarketItem({
+    name: "Item Name",
+    description: "This is a cool item.",
+    author: {},
+    location: {}
+}));
+marketItems2.push(new MarketItem({
+    name: "Item Name",
+    description: "This is a cool item.",
+    author: {},
+    location: {}
+}));
+for (var i = 0; i < marketItems2.length; i++) {
+    marketItems2[i].addToMarket("market-container-trending");
 }
