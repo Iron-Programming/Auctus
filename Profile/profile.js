3474a3e729ec165da41c634527a58f241fa51bcc
Nathan Cochran <nacochranpb@gmail.com>
2022-10-24 21:56:35 -0500

    Worked on Profile.html

--- Profile/profile.js
+++ Profile/profile.js
@@ -1,10 +1,10 @@
 
 
 /**********************************
- * User Designs
+ * User Designs Showcase
 **********************************/
 var marketItems1 = [ ];
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 5; i++) {
     marketItems1.push(new MarketItem({
         name: "Item Name",
         description: "This is a cool item.",
@@ -13,5 +13,21 @@ for (var i = 0; i < 10; i++) {
     }));
 }
 for (var i = 0; i < marketItems1.length; i++) {
-    marketItems1[i].addToMarket("user-designs");
+    marketItems1[i].addToMarket("user-designs-showcase");
+}
+
+/**********************************
+ * User Marketing Packages
+**********************************/
+var marketItems3 = [ ];
+for (var i = 0; i < 5; i++) {
+    marketItems3.push(new MarketItem({
+        name: "Item Name",
+        description: "This is a cool item.",
+        author: {},
+        location: {}
+    }));
+}
+for (var i = 0; i < marketItems3.length; i++) {
+    marketItems3[i].addToMarket("user-marketing-packages");
 }
